5c31275015ad2e5b77c93b0b80f0299f24d15d3f
Leonardo Jose da Silva Ahmed <leonardo.ahmed@unigranrio.br>
2017-01-31 11:57:21 -0200

    começando ionic [v2]

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { HomePage } from '../pages/home/home';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+
+  home: any = HomePage;
+  rootPage: any = this.home;
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+    });
+  }
+
+  openPage(page){
+    this.rootPage = page;
+  }
+}
